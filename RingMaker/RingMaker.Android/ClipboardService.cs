8364ef5d26df4503a576a19336a9c4aeee3a1c10
Darin Campbell - Raarge Actual <animusdrogai@gmail.com>
2022-05-11 22:19:38 -0400

    Added interface to add to the rune results to the clipboard.  Added the code to create the copy and paste method.

---
+++ RingMaker/RingMaker.Android/ClipboardService.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RingMaker.Interfaces;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(RingMaker.Droid.DependencyServices.ClipboardService))]
+namespace RingMaker.Droid.DependencyServices
+{
+    public class ClipboardService : IClipboardService
+    {
+        [Obsolete]
+        public void CopyToClipboard(string text)
+        {
+            var clipboardManager = (ClipboardManager)Forms.Context.GetSystemService(Context.ClipboardService);
+            ClipData clip = ClipData.NewPlainText("Android Clipboard", text);
+            clipboardManager.PrimaryClip = clip;
+        }
+    }
+}
