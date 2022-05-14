813f529e638f81136a454d6e5541644f4ddd38bd
Darin Campbell - Raarge Actual <animusdrogai@gmail.com>
2022-05-14 11:36:51 -0400

    Fixed binding for page titles to point to the view models.  Corrected the xaml for the pages to include contentpage.content which was causing the issue with the title not being read from the viewmodel.

---
+++ RingMaker/RingMaker/ViewModels/ConsumableRuneViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace RingMaker.ViewModels
+{
+    public class ConsumableRuneViewModel : BaseViewModel
+    {
+        public ConsumableRuneViewModel()
+        {
+            Title = "Consumable Runes Calculation";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
