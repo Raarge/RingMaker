8f07acefe8cba42ae185009e6cc6212ea6fc991b
Darin Campbell - Raarge Actual <animusdrogai@gmail.com>
2022-05-15 22:24:43 -0400

    Added the ConsummableRuneInfo model and the Raise Property Changed as well as the new labels, picker and entry for the Consumable Runes Calculation Page

---
+++ RingMaker/RingMaker/Models/ConsumableRuneInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace RingMaker.Models
+{
+    public class ConsumableRuneInfo : INotifyPropertyChanged
+    { 
+        private int _xpNeeded;
+        private string _runeSize;
+        private string _consumableRunesRequired;
+
+        public int XPNeeded
+        {
+            get => _xpNeeded;
+            set
+            {
+                _xpNeeded = value;
+                RaisePropertyChanged(nameof(XPNeeded));
+            }
+        }
+
+        public string RuneSize
+        {
+            get => _runeSize;
+            set
+            {
+                _runeSize = value;
+                RaisePropertyChanged(nameof(RuneSize));
+            }
+        }
+
+        public string ConsumableRunesRequired
+        {
+            get => _consumableRunesRequired;
+            set
+            {
+                _consumableRunesRequired = value;
+                RaisePropertyChanged(nameof(ConsumableRunesRequired));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- RingMaker/RingMaker/Views/ConsumableRunePage.xaml.cs
+++ RingMaker/RingMaker/Views/ConsumableRunePage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using RingMaker.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,33 @@ namespace RingMaker.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ConsumableRunePage : ContentPage
     {
+        public ConsumableRuneInfo RuneCalc { get; set; }
         public ConsumableRunePage()
         {
             InitializeComponent();
+
+            RuneCalc = new ConsumableRuneInfo
+            {
+                XPNeeded = Convert.ToInt32(xpNeeded.Text),
+                RuneSize = Convert.ToString(pkrRuneSize.SelectedItem),
+                ConsumableRunesRequired = ""
+            };
+        }
+
+        public void btnCalculateNumberOfRunes_Clicked(object sender, EventArgs e)
+        {
+            // add test to see if xpNeeded or pkrRuneSize are empty before showing hidden
+            ShowHidden();
+            RuneCalc.ConsumableRunesRequired = "Test Test Test";
+
+            lblRunesNeededResults.Text = RuneCalc.ConsumableRunesRequired;
+        }
+
+        public void ShowHidden()
+        {
+            lblRunesNeededHeader.IsVisible = true;
+            lblRunesNeededResults.IsVisible = true;
+            
         }
     }
 }
